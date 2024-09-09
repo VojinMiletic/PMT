7bb798e12aece3152ba801299866b1b2f30ee8b5
VojinMiletic <mileticvojin27@gmail.com>
2024-09-09 16:59:34 +0200

    Dodao PMT projekat

---
+++ frontend/src/app/models/Atrakcija.ts
@@ -0,0 +1,13 @@
+export class Atrakcija{
+    _id:String;
+    naziv:String;
+    koordinate:{
+        latitude:Number,
+        longitude:Number
+    };
+    opis:String;
+    drzava:String;
+    tip:String;
+    ocena:Number;
+    slika:String
+}
